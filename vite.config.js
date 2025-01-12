aab60bd902cbfd19d33e1f14d5acde9136efb4fe
hosua <hosua.o@gmail.com>
2025-01-12 14:09:06 -0500

    Idk

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/idk/",
   plugins: [react()],
-  server: {
-    proxy: {
-      "/aviation-api": {
-        target: "https://api.aviationapi.com/v1",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/aviation-api/, ""),
-      },
+  resolve: {
+    alias: {
+      "@pages": "/src/pages",
     },
   },
 });
